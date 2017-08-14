022a5e8728ff8169bb15ab891b68015e78e71ee7
Danish <dohabib@gmail.com>
2017-08-14 22:52:31 +0900

    Finished adding Japanese

--- kitewa/src/app/app.component.ts
+++ kitewa/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {TranslateService} from 'ng2-translate';
+import {Subscription} from "rxjs";
+import {Router, ActivatedRoute} from "@angular/router";
+
 
 @Component({
   selector: 'app-root',
@@ -8,22 +10,35 @@ import {TranslateService} from 'ng2-translate';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  constructor (private translate: TranslateService) {
+
+  private subscription: Subscription;
+
+  constructor (private translate: TranslateService, private activatedRoute: ActivatedRoute) {
     translate.addLangs(["en", "jp"]);
     translate.setDefaultLang("en");
 
     let browserlang = translate.getBrowserLang();
-  // translate.use(browserlang.match(/en|jp/) ? browserlang: "en")
-     translate.use('jp')
-
-
-
+    translate.use(browserlang.match(/en|jp/) ? browserlang: "en")
 
     
   }
     changeLanguage(lang){
       this.translate.use(lang);
+    }
+
+    NgOnit() {
+      this.subscription = this.activatedRoute.queryParams.subscribe(
+        (param:any) => {
+          let locale  = param ['locale'];
+          if (locale !== undefined) {
+            this.translate.use (locale)
+          }
+        }
+      )
+    }
+
+    NgOnDestroy(){
+      this.subscription.unsubscribe();
     }
 
   }
